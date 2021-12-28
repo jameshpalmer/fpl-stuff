b1197154c3d6697bf677db4c820851c6ad2eecc2
James Palmer <jameshpalmer0@gmail.com>
2021-12-28 06:04:26 +0000

    lp_tools variable classmethod added

--- solver/lp_tools.py
+++ solver/lp_tools.py
@@ -3,11 +3,12 @@
 Allows multi-dimensional arrays of objects to be processed in parallel using element-wise
 operations, with full support for pulp.LpVariable objects, unlike numpy or pandas classes.
 Arithmetic operations avaible across and between classes, and constraints can be added in
-conjunction with LpProblem (outer class for pulp.LpProblem).
+conjunction when associated with LpProblem (outer class for pulp.LpProblem).
 
     Typical usage example:
 
     #TODO
+
 """
 
 
@@ -16,11 +17,17 @@ class LpProblem:
 
 
 class LpArray:
-    pass
+    def __init__(self, data, index, prob):
+        pass
+
+    @classmethod
+    def variable():
+        pass
 
 
 class LpMatrix:
-    pass
+    def to_tensor(self):
+        pass
 
 
 class LpTensor:
