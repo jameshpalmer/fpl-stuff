8f8f026b490d22136c51a6600602dd63178bd742
James Palmer <jameshpalmer0@gmail.com>
2021-12-28 18:47:31 +0000

    Add docstring to LpArray

--- solver/Solver.py
+++ solver/Solver.py
@@ -1,7 +1,9 @@
 class Solver:
     def solve():
+        # TODO
         # If decision matrix, convert to tensor and solve diagonal
         pass
 
     def bench_solve():
+        # TODO
         pass
--- solver/lp_tools.py
+++ solver/lp_tools.py
@@ -1,9 +1,8 @@
 """Structures for linear programming variables.
 
-Allows multi-dimensional arrays of objects to be processed in parallel using element-wise
-operations, with full support for pulp.LpVariable objects, unlike numpy or pandas classes.
-Arithmetic operations avaible across and between classes, and constraints can be added in
-conjunction when associated with LpProblem (outer class for pulp.LpProblem).
+Allows multi-dimensional arrays of objects to be processed in parallel using element-wise operations, with full support
+for pulp.LpVariable objects, unlike numpy or pandas classes. Arithmetic operations avaible across and between classes,
+and constraints can be added when associated with LpProblem (outer class for pulp.LpProblem).
 
     Typical usage example:
 
@@ -21,12 +20,33 @@ class LpProblem:
 
 
 class LpArray:
+    """1-Dimensional array-like structure with support for pulp.LpVariable objects, and support for applying
+    element-wise PuLP constraints when associated with LpProblem.
+    """
+
     def __init__(self, data: ArrayLike = None, index: ArrayLike = None, prob: LpProblem = None):
+        """This class models an array with the following parameters:
+
+        Args:
+            data (ArrayLike, optional): Values contained in LpArray. Defaults to None.
+            index (ArrayLike, optional): Paired indices of values. Defaults to None.
+            prob (LpProblem, optional): Associated LpProblem object for constraint application. Defaults to None.
+        """
         pass
 
     @classmethod
+    def from_dict(cls, data: dict = None, prob: LpProblem = None):
+        """Initialise LpArray with data from dict, with the following parameters:
+
+        Args:
+            data (dict, optional): dict (length n) object containing {index[0]: values[0], index[1]: values[1], ..., \
+                 index[1]: values[n]}. Defaults to None.
+            prob (LpProblem, optional): [description]. Defaults to None.
+        """
+
+    @classmethod
     def variable(cls, name: str, index: ArrayLike = None, lower: float = None, upper: float = None, cat: type = None):
-        """Create LpArray containing pulp.LpVariable objects.
+        """Initialise LpArray containing pulp.LpVariable objects, with the following parameters:
 
         Args:
             name (str): Name for pulp.LpVariable
@@ -48,5 +68,6 @@ class LpTensor:
 
 
 if __name__ == '__main__':
-    lp = pulp.LpVariable('lp', )
+    lp = pulp.LpVariable()
     LpArray.variable
+    LpArray.from_dict
